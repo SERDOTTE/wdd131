326cbbb26e2956ef4eafe8a5e433ea6cadd8d0da
serdotte <rodrigo.serdotte@gmail.com>
2025-01-28 06:40:54 -0300

    version 008

--- scripts/place.js
+++ scripts/place.js
@@ -3,3 +3,16 @@ document.getElementById("copyrightYear").textContent = currentYear;
 
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = lastModified;
+
+function calculateWindChill(temperature, windSpeed) {
+    if (temperature <= 10 && windSpeed > 4.8) {
+        return (
+        13.12 +
+        0.6215 * temperature -
+        11.37 * Math.pow(windSpeed, 0.16) +
+        0.3965 * temperature * Math.pow(windSpeed, 0.16)
+      ).toFixed(2); 
+    } else {
+      return "N/A";
+    }
+  }
