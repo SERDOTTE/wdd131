5ce62811967d94bac13d0c752aa1ee187ce79edb
serdotte <rodrigo.serdotte@gmail.com>
2025-01-29 22:27:39 -0300

    javascript atualization

--- scripts/place.js
+++ scripts/place.js
@@ -11,8 +11,29 @@ function calculateWindChill(temperature, windSpeed) {
         0.6215 * temperature -
         11.37 * Math.pow(windSpeed, 0.16) +
         0.3965 * temperature * Math.pow(windSpeed, 0.16)
-      ).toFixed(2); 
+      ).toFixed(1); 
     } else {
       return "N/A";
     }
   }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    let temperature = 10; // Defina a temperatura
+    let windSpeed = 5; // Defina a velocidade do vento
+    let windChillElement = document.querySelector(".weather-card-description p:nth-child(4)"); 
+
+    if (windChillElement) {
+        windChillElement.innerHTML = `<strong>Wind Chill:</strong> ${calculateWindChill(temperature, windSpeed)}`;
+    }
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  let temperature = 10; // Defina a temperatura
+  let windSpeed = 5; // Defina a velocidade do vento
+  let windChillElement = document.querySelector(".weather-card-description-mobile p:nth-child(4)"); 
+
+  if (windChillElement) {
+      windChillElement.innerHTML = `<strong>Wind Chill:</strong> ${calculateWindChill(temperature, windSpeed)}`;
+  }
+});
